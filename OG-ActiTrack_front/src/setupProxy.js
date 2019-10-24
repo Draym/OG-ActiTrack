e2dc577e60a3d0481c7c32f7244c52baa22ddf43
Draym <kevin.draym@gmail.com>
2019-10-24 17:15:01 +0100

    [Correct] proxy generic

--- OG-ActiTrack_front/src/setupProxy.js
+++ OG-ActiTrack_front/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://localhost:9292' }));
+  app.use(proxy(process.env.REACT_APP_API, { target: process.env.REACT_APP_SERVER_URL }));
 };
